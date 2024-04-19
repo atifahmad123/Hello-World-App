bf1888a291722596823f830184b4228e58763f52
ATIF AHMAD QURESHI <153423923+atifahmad123@users.noreply.github.com>
2024-04-19 09:16:50 +0500

    Add files via upload

---
+++ Hello World App.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    using namespace std;/* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a;
+	 cout<<"Enter a number to print Hello World."<<endl;
+    
+    
+    cin>> a;
+    cout<<"Hello World"<<endl;
+    return 0;
+}
